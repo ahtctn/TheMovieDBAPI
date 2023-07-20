5aa20a933f1a9897bf41b9c358feb833dd51d93b
Ahmet Ali Çetin <ahtctn@gmail.com>
2023-07-20 18:58:32 +0300

    Initial Commit

---
+++ TheMovieDBAPI/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  TheMovieDBAPI
+//
+//  Created by Ahmet Ali ÇETİN on 20.07.2023.
+//
+
+import Foundation
+
+enum Constants {
+    static let API_KEY: String = "895a5c6345c566edc5a0b8066c3f62ec"
+    
+    static let baseURL: String = "https://api.themoviedb.org/3"
+}
---
+++ TheMovieDBAPI/View/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieDBAPI
+//
+//  Created by Ahmet Ali ÇETİN on 20.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
